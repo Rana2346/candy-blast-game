5cd4a3ae4bb74aea8ef4235380b72e8e4f8c7fae
Rana2346 <rf6853162@gmail.com>
2026-07-12 20:37:03 +0500

    Refactor game.js to create game board and check matches

--- game.js
+++ game.js
@@ -1,18 +1,42 @@
-const colors = ["red", "blue", "green", "yellow", "purple"];
 const board = document.getElementById("game");
+const scoreDisplay = document.getElementById("score");
 
+const width = 8;
+const colors = ["red", "blue", "green", "yellow", "purple"];
+let squares = [];
 let score = 0;
 
-for (let i = 0; i < 64; i++) {
-  let candy = document.createElement("div");
-  candy.classList.add("candy");
-  candy.style.background = colors[Math.floor(Math.random() * colors.length)];
-
-  candy.onclick = function () {
-    score += 10;
-    document.getElementById("score").innerHTML =
-      "Score: " + score;
-  };
-
-  board.appendChild(candy);
+function createBoard() {
+  for (let i = 0; i < width * width; i++) {
+    const square = document.createElement("div");
+    square.classList.add("candy");
+    let color = colors[Math.floor(Math.random() * colors.length)];
+    square.style.background = color;
+    square.setAttribute("data-color", color);
+    board.appendChild(square);
+    squares.push(square);
+  }
 }
+
+function checkMatches() {
+  for (let i = 0; i < squares.length; i++) {
+    let color = squares[i].getAttribute("data-color");
+
+    if (
+      i % width < width - 2 &&
+      squares[i + 1].getAttribute("data-color") === color &&
+      squares[i + 2].getAttribute("data-color") === color
+    ) {
+      score += 30;
+      scoreDisplay.innerHTML = "Score: " + score;
+
+      squares[i].style.background = "white";
+      squares[i + 1].style.background = "white";
+      squares[i + 2].style.background = "white";
+    }
+  }
+}
+
+createBoard();
+
+setInterval(checkMatches, 1000);
